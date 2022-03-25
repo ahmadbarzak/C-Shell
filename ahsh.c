3c5a9f319979c0183cf41e0b7c3b5be04e6b8d6e
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2022-03-25 23:13:19 +1300

    Implemented Change Directory command to ahsh.c

--- ahsh.c
+++ ahsh.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+
+
+int containsDirectory(char* line, char* path)
+{
+    int k = 4;
+    int hasCommand = 0;
+    while(line[k] != '\0')
+    {
+        if(line[k] != ' ')
+        {
+            return 1;
+        }
+        k++;
+    }
+    return 0;
+}
+
+void changeDirectory(char* line, char* path)
+{
+    int j = 3;
+    int hasCommand = containsDirectory(line, path);
+    if (hasCommand == 1)
+    {
+        while(line[j] != '\0')
+        {
+            path[j-3] = line[j];
+            j++;
+        }
+        chdir(path);
+    }
+    else
+    {
+        chdir("/home");
+    }
+}
+
+
 
 int main(int argc, char *argv[]){
     
@@ -15,29 +54,40 @@ int main(int argc, char *argv[]){
         size_t size;
         size_t characters = getline(&line, &size, stdin);
         line[strlen(line) - 1] = '\0';
-        char delim[] = " ";
-        char *tokens[100];
-        int i = 0;
-        tokens[i] = strtok(line, delim);
-        while(tokens[i] != NULL)
+        char *path = malloc(100*sizeof(char));
+        if(strcmp(line, "cd") == 0)
         {
-            i++;
-            tokens[i] = strtok(NULL, delim);
+            chdir("/home");
         }
-        if (!strcmp(tokens[0], "exit"))
+        else if (strncmp(line, "cd ", 3) == 0)
         {
-            return 0;
-        }
-        
-        pid_t id = fork();
-        if (id == 0)
-        {
-            execvp(tokens[0], tokens);
+            changeDirectory(line, path);
         }
         else
         {
-            pid_t pid = waitpid(id, NULL, 0);
+            char delim[] = " ";
+            char **tokens = malloc(100*sizeof(char*));
+            int i = 0;
+            tokens[i] = strtok(line, delim);
+            while(tokens[i] != NULL)
+            {
+                i++;
+                tokens[i] = strtok(NULL, delim);
+            }
+            pid_t id = fork();
+            if (id == 0)
+            {
+                execvp(tokens[0], tokens);
+            }
+            else
+            {
+                pid_t pid = waitpid(id, NULL, 0);
+            }
+            free(tokens);
         }
+        free(line);
+        free(path);
     }
     return 0;
 }
+
